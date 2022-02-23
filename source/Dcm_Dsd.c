758266b14b703bb7159fc8bb9b02fbd5d716bba5
AamerElbaz <55632370+AamerElbaz@users.noreply.github.com>
2022-02-23 21:40:20 +0200

    Update Dcm_Dsd.c

--- source/Dcm_Dsd.c
+++ source/Dcm_Dsd.c
@@ -10,7 +10,7 @@
 #include "include/Dcm_Dsd.h"
 #include "include/Dcm_Dsp.h"
 
-
+uint8 x,y;
 /**********************************
  * DSD-STATIC-FUNTIONS-PROTOTYPES *
  **********************************/
